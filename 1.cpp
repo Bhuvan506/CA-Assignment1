d0c3671767a35f93a81fc443b655bf161298145e
Tejdeep2003 <77499201+Tejdeep2003@users.noreply.github.com>
2021-04-28 19:05:23 +0530

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -30,7 +30,7 @@ void decode_execute_ins(long int IR, long int MAR)
 			{
 				AC = memory[MAR];
 				printf("LOAD M(x) function is activated\n");
-				printf("%ld is loaded \n",AC);
+				printf("%ld is loaded to accumulator \n",AC);
 				break;
 			}
 		case 2:
@@ -108,6 +108,23 @@ void decode_execute_ins(long int IR, long int MAR)
 				printf("%ld is loaded to Accumulator \n",AC);
 				break;
 			}
+		case 11:
+			{
+				PC = memory[MAR];
+				JUMP = 1;
+				printf("JUMP M(x,0:19) function is activated\n");
+				printf("next instruction is fetched from location %ld\n",PC);
+				break;
+			}
+		case 12:
+			{
+				PC = memory[MAR];
+				JUMP_RIGHT = 1;
+				JUMP = 1;
+				printf("JUMP M(x,20:39) function is activated\n");
+				printf("next instruction is fetched from location %ld\n",PC);
+				break;
+			}
 
 
 	}
@@ -120,61 +137,117 @@ int fetch()
 {
 	int n;
 	
-	MBR = memory[PC];
-	printf("The instruction in memory location of PC is sent to MBR \n");
 	
-	if( MBR > 1048576 && MBR < 1099511627776)
+	if(JUMP_RIGHT == 0)
 	{
-		printf("left instruction is present \n");
-		
-		IR = MBR/4294967296;
-		printf("The opcode of left instruction is loaded into IR \n");
-		
-		MAR = (MBR/1048576)%4096;
-		printf("The address of the desired value is loaded into MAR \n");
-		
-		IBR = MBR % 1048576;
-		printf("right instruction is loaded into MBR \n");
+		MBR = memory[PC];
+		printf("The instruction in memory location of PC is sent to MBR \n");
 
-		if(run == 1)
+		if( MBR >= 1048576 && MBR < 1099511627776 )
 		{
-			PC += 1;
-			printf("\n");
-			printf("PC is incremented\n");
-			printf("\n");
-		}
-
-		printf("the opcode is sent for decoding and followed by execution of the instruction \n");
-		decode_execute_ins(IR,MAR);
-		printf("\n");
-
-		if(JUMP == 0)
-		{
-			IR = IBR/4096;
-			printf("The opcode of right instruction is loaded into IR \n");
-	
-			MAR = IBR % 4096;
+			printf("left instruction is present \n");
+		
+			IR = MBR/4294967296;
+			printf("The opcode of left instruction is loaded into IR \n");
+		
+			MAR = (MBR/1048576)%4096;
 			printf("The address of the desired value is loaded into MAR \n");
 		
+			IBR = MBR % 1048576;
+			printf("right instruction is loaded into MBR \n");
+
+			if(run == 1)
+			{
+				PC += 1;
+				printf("PC is incremented\n");
+			}
+
+			printf("the opcode is sent for decoding and followed by execution of the instruction \n");
+			decode_execute_ins(IR,MAR);
+			printf("\n");
+
+			if(JUMP == 0 && run == 1)
+			{
+				IR = IBR/4096;
+				printf("The opcode of right instruction is loaded into IR \n");
+
+	
+				MAR = IBR % 4096;
+				printf("The address of the desired value is loaded into MAR \n");
+		
+				printf("the opcode is sent for decoding and followed by execution of the instruction \n");
+				decode_execute_ins(IR,MAR);
+				printf("\n");
+			}
+			JUMP = 0;
+		}
+		else 
+		{
+
+			printf("There is no left instruction");
+		
+			IR = MBR/4096;
+			printf("The opcode of right instruction is loaded into IR \n");
+		
+			MAR = MBR % 4096;
+			printf("The address of the desired value is loaded into MAR \n");
+
+			if(run == 1)
+			{
+				PC += 1;
+				printf("PC is incremented\n");
+			}
+
 			printf("the opcode is sent for decoding and followed by execution of the instruction \n");
 			decode_execute_ins(IR,MAR);
 			printf("\n");
 		}
-		JUMP = 0;
 	}
 	else
 	{
-		printf("There is no left instruction \n");
-		
-		IR = MBR/4096;
-		printf("The opcode of right instruction is loaded into IR \n");
-		
-		MAR = MBR % 4096;
-		printf("The address of the desired value is loaded into MAR \n");
+		if( MBR >= 1048576 && MBR < 1099511627776 )
+		{
+			MBR = (memory[PC]) % 1048576;
+			printf("The instruction in memory location of PC is sent to MBR \n");
 
-		printf("the opcode is sent for decoding and followed by execution of the instruction \n");
-		decode_execute_ins(IR,MAR);
-		printf("\n");
+			IR = MBR/4096;
+			printf("The opcode of right instruction is loaded into IR \n");
+
+			MAR = MBR % 4096;
+			printf("The address of the desired value is loaded into MAR \n");
+
+			if(run == 1)
+			{
+				PC += 1;
+				printf("PC is incremented\n");
+			}
+
+			printf("the opcode is sent for decoding and followed by execution of the instruction \n");
+			decode_execute_ins(IR,MAR);
+			printf("\n");
+		}
+		else
+		{
+			MBR = memory[PC];
+			printf("The instruction in memory location of PC is sent to MBR \n");
+
+			IR = MBR/4096;
+			printf("The opcode of right instruction is loaded into IR \n");
+
+			MAR = MBR % 4096;
+			printf("The address of the desired value is loaded into MAR \n");
+
+			if(run == 1)
+			{
+				PC += 1;
+				printf("PC is incremented\n");
+			}
+
+			printf("the opcode is sent for decoding and followed by execution of the instruction \n");
+			decode_execute_ins(IR,MAR);
+			printf("\n");
+		}
+		JUMP_RIGHT = 0;
 	}
 
 
@@ -183,9 +256,10 @@ int fetch()
 
 int main()
 {
-	memory[0] = 0x0102002010;
+	memory[0] = 0x0C02002010;
 	memory[1] = 0x090300A000;
-	memory[32] = 5;
+	memory[2] = 0x000300A000;
+	memory[32] = 1;
 	memory[16] = 6;
 	memory[48] = 7;
 
