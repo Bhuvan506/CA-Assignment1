4ab625b4ef2e937e3540c610cc01020778f58d91
Bhuvan <jsbhuvan@gmail.com>
2021-04-29 10:54:26 +0530

    small changes.

--- 2.cpp
+++ 2.cpp
@@ -30,14 +30,14 @@ void decode_execute_ins(long int IR, long int MAR)
 			{
 				AC = memory[MAR];
 				cout << "LOAD M(x) function is activated" << endl;
-				cout << AC << " is loaded to accumulator " << endl;
+				cout << AC << " is loaded to accumulator. " << endl;
 				break;
 			}
 		case 2:
 			{
 				AC = -(memory[MAR]);
 				cout << "LOAD -M(x) function is activated" << endl;
-				cout << AC << " is loaded to accumulator " << endl;
+				cout << AC << " is loaded to accumulator. " << endl;
 				break;
 			}
 		case 3:
@@ -47,7 +47,7 @@ void decode_execute_ins(long int IR, long int MAR)
 				else
 					AC = -(memory[MAR]);
 				cout << "LOAD |M(x)| function is activated" << endl;
-				cout << AC << " is loaded to accumulator " << endl;;
+				cout << AC << " is loaded to accumulator. " << endl;;
 				break;
 			}
 		case 4:
@@ -57,21 +57,21 @@ void decode_execute_ins(long int IR, long int MAR)
 				else
 					AC = memory[MAR];
 				cout << "LOAD -|M(x)| function is activated" << endl;
-				cout << AC << " is loaded to accumulator " << endl;
+				cout << AC << " is loaded to accumulator. " << endl;
 				break;
 			}
 		case 5:
 			{
 				AC = AC + memory[MAR];
 				cout << "ADD M(x) function is activated" << endl;
-				cout << "The resultant sum is " << AC << endl;
+				cout << "The resultant sum is " << AC << "." << endl;
 				break;
 			}
 		case 6:
 			{
 				AC = AC - memory[MAR];
 				cout << "SUB M(x) function is activated" << endl;
-				cout << "The resultant difference is " << AC << endl;
+				cout << "The resultant difference is " << AC << "." << endl;
 				break;
 			}
 		case 7:
@@ -81,7 +81,7 @@ void decode_execute_ins(long int IR, long int MAR)
 				else
 					AC = AC - memory[MAR];
 				cout << "ADD |M(x)| function is activated" << endl;
-				cout << "The resultant sum is " << AC << endl;
+				cout << "The resultant sum is " << AC << "." << endl;
 				break;
 			}
 		case 8:
@@ -91,7 +91,7 @@ void decode_execute_ins(long int IR, long int MAR)
 				else
 					AC = AC + memory[MAR];
 				cout << "SUB |M(x)| function is activated" << endl;
-				cout << "The resultant difference is " << AC<< endl;
+				cout << "The resultant difference is " << AC << "." << endl;
 				break;
 			}
 		case 9:
@@ -119,14 +119,14 @@ void decode_execute_ins(long int IR, long int MAR)
 				MQ = AC / memory[MAR];   		//Quotient
 				AC = AC % memory[MAR];   		//Remainder
 				cout << "DIV M(x) function is activated" << endl;
-				cout << "The resultant qoutient of the division is " << MQ << endl;
+				cout << "The resultant qoutient of the division is " << MQ << "." << endl;
 			}
 		case 13:
 			{
 				PC = memory[MAR];
 				JUMP = 1;
 				cout << "JUMP M(x,0:19) function is activated" << endl;
-				cout << "Next instruction is fetched from location. " << PC << endl;
+				cout << "Next instruction is fetched from location " << PC << "." << endl;
 				break;
 			}
 		case 14:
@@ -135,7 +135,7 @@ void decode_execute_ins(long int IR, long int MAR)
 				JUMP_RIGHT = 1;
 				JUMP = 1;
 				cout << "JUMP M(x,20:39) function is activated" << endl;
-				cout << "Next instruction is fetched from location. " << PC << endl;
+				cout << "Next instruction is fetched from location " << PC << "." << endl;
 				break;
 			}
 		case 15:
@@ -143,7 +143,7 @@ void decode_execute_ins(long int IR, long int MAR)
 				if(AC >= 0){
 					JUMP = 1;
 					cout << "JUMP+ M(x,0:19) function is activated" << endl;
-					cout << "Next instruction is fetched from location. " << PC << endl;
+					cout << "Next instruction is fetched from location " << PC << "." << endl;
 				}
 				break;
 			}
@@ -153,7 +153,7 @@ void decode_execute_ins(long int IR, long int MAR)
 					JUMP_RIGHT = 1;
 					JUMP = 1;
 					cout << "JUMP+ M(x,20:39) function is activated" << endl;
-					cout << "Next instruction is fetched from location. " << PC << endl;
+					cout << "Next instruction is fetched from location " << PC << "." << endl;
 				}
 				break;
 			}
@@ -168,6 +168,12 @@ void decode_execute_ins(long int IR, long int MAR)
 				AC = AC/2;
 				cout << "RSH function is activated" << endl;
 				cout << "Accumulator is right shifted by one bit." << endl;
+			}
+		case 33:
+			{
+				memory[MAR] = AC;
+				cout << "STOR M(x) function is activated" << endl;
+				cout << "Transfered contents of accumulator to memory location " << MAR << "." << endl;
 			}
 
 
@@ -185,41 +191,41 @@ int fetch()
 	if(JUMP_RIGHT == 0)
 	{
 		MBR = memory[PC];
-		cout << "The instruction in memory location of PC is sent to MBR" << endl;
+		cout << "The instruction in memory location of PC is sent to MBR." << endl;
 
 		if( MBR >= 1048576 && MBR < 1099511627776 )
 		{
-			cout << "left instruction is present" << endl;
+			cout << "Left instruction is present." << endl;
 		
 			IR = MBR/4294967296;
-			cout << "The opcode of left instruction is loaded into IR" << endl;
+			cout << "The opcode of left instruction is loaded into IR." << endl;
 		
 			MAR = (MBR/1048576)%4096;
-			cout << "The address of the desired value is loaded into MAR" << endl;
+			cout << "The address of the desired value is loaded into MAR." << endl;
 		
 			IBR = MBR % 1048576;
-			cout << "right instruction is loaded into MBR" << endl;
+			cout << "Right instruction is loaded into MBR." << endl;
 
 			if(run == 1)
 			{
 				PC += 1;
-				cout << "PC is incremented" << endl;
+				cout << "PC is incremented." << endl;
 			}
 
-			cout << "the opcode is sent for decoding and followed by execution of the instruction" << endl;
+			cout << "The opcode is sent for decoding and followed by execution of the instruction." << endl;
 			decode_execute_ins(IR,MAR);
 			cout << "" << endl;
 
 			if(JUMP == 0 && run == 1)
 			{
 				IR = IBR/4096;
-				cout << "The opcode of right instruction is loaded into IR" << endl;
+				cout << "The opcode of right instruction is loaded into IR." << endl;
 
 	
 				MAR = IBR % 4096;
-				cout << "The address of the desired value is loaded into MAR" << endl;
+				cout << "The address of the desired value is loaded into MAR." << endl;
 		
-				cout << "the opcode is sent for decoding and followed by execution of the instruction" << endl;
+				cout << "The opcode is sent for decoding and followed by execution of the instruction." << endl;
 				decode_execute_ins(IR,MAR);
 				cout << "" << endl;
 			}
@@ -228,21 +234,21 @@ int fetch()
 		else 
 		{
 
-			cout << "There is no left instruction" << endl;
+			cout << "There is no left instruction." << endl;
 		
 			IR = MBR/4096;
-			cout << "The opcode of right instruction is loaded into IR" << endl;
+			cout << "The opcode of right instruction is loaded into IR." << endl;
 		
 			MAR = MBR % 4096;
-			cout << "The address of the desired value is loaded into MAR" << endl;
+			cout << "The address of the desired value is loaded into MAR." << endl;
 
 			if(run == 1)
 			{
 				PC += 1;
-				cout << "PC is incremented" << endl;
+				cout << "PC is incremented." << endl;
 			}
 
-			cout << "the opcode is sent for decoding and followed by execution of the instruction" << endl;
+			cout << "The opcode is sent for decoding and followed by execution of the instruction." << endl;
 			decode_execute_ins(IR,MAR);
 			cout << "" << endl;
 		}
@@ -252,42 +258,42 @@ int fetch()
 		if( MBR >= 1048576 && MBR < 1099511627776 )
 		{
 			MBR = (memory[PC]) % 1048576;
-			cout << "The instruction in memory location of PC is sent to MBR" << endl;
+			cout << "The instruction in memory location of PC is sent to MBR." << endl;
 
 			IR = MBR/4096;
-			cout << "The opcode of right instruction is loaded into IR" << endl;
+			cout << "The opcode of right instruction is loaded into IR." << endl;
 
 			MAR = MBR % 4096;
-			cout << "The address of the desired value is loaded into MAR" << endl;
+			cout << "The address of the desired value is loaded into MAR." << endl;
 
 			if(run == 1)
 			{
 				PC += 1;
-				cout << "PC is incremented" << endl;
+				cout << "PC is incremented." << endl;
 			}
 
-			cout << "the opcode is sent for decoding and followed by execution of the instruction" << endl;
+			cout << "The opcode is sent for decoding and followed by execution of the instruction." << endl;
 			decode_execute_ins(IR,MAR);
 			cout << "" << endl;
 		}
 		else
 		{
 			MBR = memory[PC];
-			cout << "The instruction in memory location of PC is sent to MBR" << endl;
+			cout << "The instruction in memory location of PC is sent to MBR." << endl;
 
 			IR = MBR/4096;
-			cout << "The opcode of right instruction is loaded into IR" << endl;
+			cout << "The opcode of right instruction is loaded into IR." << endl;
 
 			MAR = MBR % 4096;
-			cout << "The address of the desired value is loaded into MAR" << endl;
+			cout << "The address of the desired value is loaded into MAR." << endl;
 
 			if(run == 1)
 			{
 				PC += 1;
-				cout << "PC is incremented" << endl;
+				cout << "PC is incremented." << endl;
 			}
 
-			cout << "the opcode is sent for decoding and followed by execution of the instruction" << endl;
+			cout << "The opcode is sent for decoding and followed by execution of the instruction." << endl;
 			decode_execute_ins(IR,MAR);
 			cout << "" << endl;
 		}
